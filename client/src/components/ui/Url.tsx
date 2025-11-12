265ec0b39f7e11ed0c4291ad7b9c46e8dda99372
omar <omarqais225@gmail.com>
2025-11-12 18:04:39 +0200

    create opacity animation for texts links in dashboards

--- client/src/components/ui/Url.tsx
+++ client/src/components/ui/Url.tsx
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
 import useSlideStore from "../../store/SlideStore";
-
-const Url = ({ isActive, direct, text, icon }: {
+const Url = ({
+    isActive,
+    direct,
+    text,
+    icon
+}: {
     isActive: boolean,
     direct: string,
     text: string,
@@ -9,15 +13,22 @@ const Url = ({ isActive, direct, text, icon }: {
 }) => {
     const { isSlideOpen } = useSlideStore();
     return (
-        <Link to={direct} className={`text-black gap-2 py-3 font-nunito flex w-full items-start justify-start hover:bg-purple-1 ${isActive && 'bg-purple-1 border-r-[3px] border-r-purple-2'} duration-300 transition-all`}>
+        <Link
+            to={direct}
+            className={`text-black gap-2 py-3 font-nunito flex w-full items-start justify-start hover:bg-purple-1 ${isActive && 'bg-purple-1 border-r-[3px] border-r-purple-2'
+                } duration-300 transition-all`}
+        >
+
             <div className="pl-5 flex gap-2 items-center justify-center">
                 <img src={icon} className="w-5" alt={text} />
-                <span className={`${isSlideOpen ? 'block' : 'hidden'} max-md:hidden duration-300 transition-all`}>
+                <span className={`${isSlideOpen
+                    ? 'opacity-100 max-w-[200px]'
+                    : 'opacity-0 max-w-0'
+                    } max-md:hidden duration-300 transition-all overflow-hidden whitespace-nowrap`}>
                     {text}
                 </span>
             </div>
         </Link>
     );
 }
-
 export default Url;
