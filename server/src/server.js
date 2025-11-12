b886b142040b2465e03b569ee256bff971b4cc6a
omar <omarqais225@gmail.com>
2025-11-12 21:37:37 +0200

    create cron for making server wake up always and arcjet for making server safety from dos attack

--- server/src/server.js
+++ server/src/server.js
@@ -3,8 +3,46 @@ import express, { request, response } from 'express';
 import { createServer } from "http";
 import chalk from 'chalk';
 import { connectDB } from "./lib/db.js";
+import { aj } from './lib/aj.js';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import job from './config/cron.js';
 const app = express();
+if (process.env.NODE_ENV === 'production') job.start();
 app.use(express.json());
+app.use(cors({
+    origin: process.env.NODE_ENV === 'development'
+        ? 'http://localhost:5173'
+        : '',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}));
+app.use(helmet({
+    contentSecurityPolicy: false,
+}));
+app.use(morgan('dev'));
+app.use(async (req, res, next) => {
+    try {
+        const decision = await aj.protect(req, { requested: 1 });
+        if (decision.isDenied()) {
+            if (decision.reason.isRateLimit()) {
+                return res.status(429).json({ error: 'Rate limit exceeded, Too many requests!' });
+            } else if (decision.reason.isBot()) {
+                return res.status(403).json({ error: 'Bot access denied!' });
+            } else {
+                return res.status(403).json({ error: 'Forbidden' });
+            }
+        }
+        if (decision.results.some(result => result.reason.isBot() && result.reason.isSpoofed())) {
+            return res.status(403).json({ error: 'Spoofed bot detected!' });
+        }
+        next();
+    } catch (error) {
+        console.error(error.message);
+        next(error);
+    }
+});
 app.get('/cron', (request, response) => {
     return response.status(200).json({
         message: 'Server connected successfully!!'
