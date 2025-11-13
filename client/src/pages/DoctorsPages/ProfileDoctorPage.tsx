ab545c624168c7a3d609afad5957b9c3489e01b3
omar <omarqais225@gmail.com>
2025-11-13 17:01:54 +0200

    finish appotintments doctors page

--- client/src/pages/DoctorsPages/ProfileDoctorPage.tsx
+++ client/src/pages/DoctorsPages/ProfileDoctorPage.tsx
@@ -1,6 +1,6 @@
 const ProfileDoctorPage = () => {
     return (
-        <div>
+        <div className="p-5">
             <h1 className="text-3xl font-bold text-black">Profile</h1>
         </div>
     );
