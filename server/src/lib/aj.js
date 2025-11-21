eb0e5f0b52fa3be002be12dcd74f7c45258e67e5
omar <omarqais225@gmail.com>
2025-11-21 21:05:53 +0200

    last try

--- server/src/lib/aj.js
+++ server/src/lib/aj.js
@@ -2,7 +2,7 @@ import arcjet, { tokenBucket, shield, detectBot } from "@arcjet/node";
 import 'dotenv/config';
 export const aj = arcjet({
     key: process.env.ARCJET_KEY,
-    characteristics: ["ip.src"],
+    characteristics: process.env.NODE_ENV === 'development' ? ["ip.src"] : [process.env.MISSING_IP_ADDRESS],
     rules: [
         shield({
             mode: "LIVE"
