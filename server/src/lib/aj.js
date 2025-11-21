45ddaa9be8ff635a519492ba0d112090c87eb08a
omar <omarqais225@gmail.com>
2025-11-21 20:41:24 +0200

    try to fix arcjet again

--- server/src/lib/aj.js
+++ server/src/lib/aj.js
@@ -1,13 +1,8 @@
 import arcjet, { tokenBucket, shield, detectBot } from "@arcjet/node";
 import 'dotenv/config';
 const isProduction = process.env.NODE_ENV === 'production';
-const isDevelopment = process.env.NODE_ENV === 'development';
 const getCharacteristics = () => {
-    if (isProduction) {
-        return ["ip.src", "userAgent", "requestPath"];
-    } else {
-        return ["userAgent", "requestPath", "method"];
-    }
+    return ["userAgent", "requestPath", "method"];
 };
 const getMode = () => {
     return isProduction ? "LIVE" : "DRY_RUN";
