0868b4ce0cde32399131a8d354fd97f78d9a63ed
omar <omarqais225@gmail.com>
2025-11-21 20:26:08 +0200

    fix dist location

--- server/src/server.js
+++ server/src/server.js
@@ -71,9 +71,9 @@ app.use(userAppointmentRoute);
 app.use(doctorAppointmentRoute);
 app.use(adminAppointmentRoute);
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/dist")));
+    app.use(express.static(path.join(__dirname, "client", "dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
     });
 }
 const server = createServer(app);
