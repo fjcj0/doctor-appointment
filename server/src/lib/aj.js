98fd5ac332d4f603f2ac7ca4fd9b04d395f4559b
omar <omarqais225@gmail.com>
2025-11-21 21:09:52 +0200

    error cannot solve maybe render block arcjet

--- server/src/lib/aj.js
+++ server/src/lib/aj.js
@@ -2,7 +2,7 @@ import arcjet, { tokenBucket, shield, detectBot } from "@arcjet/node";
 import 'dotenv/config';
 export const aj = arcjet({
     key: process.env.ARCJET_KEY,
-    characteristics: process.env.NODE_ENV === 'development' ? ["ip.src"] : [process.env.MISSING_IP_ADDRESS],
+    characteristics: ["ip.src"],
     rules: [
         shield({
             mode: "LIVE"
