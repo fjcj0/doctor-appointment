2831a64a5c423e63cd486e8f84f8cb495f489a9e
omar <omarqais225@gmail.com>
2025-11-21 21:13:26 +0200

    fix arcjet again

--- server/src/lib/aj.js
+++ server/src/lib/aj.js
@@ -1,9 +1,13 @@
-import arcjet, { tokenBucket, shield, detectBot } from "@arcjet/node";
+import arcjet, { tokenBucket, shield, detectBot, allow } from "@arcjet/node";
 import 'dotenv/config';
 export const aj = arcjet({
     key: process.env.ARCJET_KEY,
     characteristics: ["ip.src"],
     rules: [
+        allow({
+            mode: "LIVE",
+            ip: ["74.220.48.250"]
+        }),
         shield({
             mode: "LIVE"
         }),
